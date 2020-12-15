3d1391fd64cc4002387277a1d5a51adb0d969549
HamsterCat-W <2756878164@qq.com>
2020-12-15 21:53:01 +0800

    vuex的admin文件

--- stack-client/src/vuex/admin/mutation.js
+++ stack-client/src/vuex/admin/mutation.js
@@ -1,18 +1,33 @@
 const mutation = {
-  updateSessionStorage(key, value) {
-    if (typeof key === "object") {
-      let obj = key;
-      Object.keys().forEach((item) => {
-        sessionStorage.setItem(item, obj[item]);
-      });
+    updateSessionStorage(key, value) {
+        if (typeof key === "object") {
+            let obj = key;
+            Object.keys().forEach((item) => {
+                sessionStorage.setItem(item, obj[item]);
+            });
+        }
+        if (typeof key === "string") {
+            sessionStorage.setItem(key, value);
+        }
+    },
+    changeState(state, params) {
+        state.temp = params;
+    },
+    getClassTable(state, payload) {
+        // console.log(payload);
+        state.classTable = payload;
+
+    },
+    getSchoolInfo(state, payload) {
+        state.schoolInfo = payload;
+    },
+    getSubOrgId(state, payload) {
+        for (let item of state.schoolInfo) {
+            if (item.subOrgName === payload) {
+                state.subOrgId = item._id;
+            }
+        }
     }
-    if (typeof key === "string") {
-      sessionStorage.setItem(key, value);
-    }
-  },
-  changeState(state, params) {
-    state.temp = params;
-  },
 };
 
 export default mutation;
